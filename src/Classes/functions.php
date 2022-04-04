d737c5ba33a1b3b10851b2bf93b752acf72e51c8
Ruben Robles <d8vjork@outlook.com>
2022-04-04 23:20:43 +0200

    wip

--- src/Classes/functions.php
+++ src/Classes/functions.php
@@ -2,48 +2,42 @@
 
 namespace OpenSoutheners\LaravelHelpers\Classes;
 
-if (! function_exists('class_namespace')) {
-    /**
-     * Get namespace from class string or object.
-     *
-     * @param object|string $class
-     * @return string
-     */
-    function class_namespace($class)
-    {
-        $classArr = explode('\\', is_object($class) ? get_class($class) : $class);
+/**
+ * Get namespace from class string or object.
+ *
+ * @param object|string $class
+ * @return string
+ */
+function class_namespace($class)
+{
+    $classArr = explode('\\', is_object($class) ? get_class($class) : $class);
 
-        array_pop($classArr);
+    array_pop($classArr);
 
-        return implode('\\', $classArr);
-    }
+    return implode('\\', $classArr);
 }
 
-if (! function_exists('class_implement')) {
-    /**
-     * Check if class instance or string uses an specific interface.
-     *
-     * @param object|string $class
-     * @param string $interface
-     * @return bool
-     */
-    function class_implement($class, string $interface)
-    {
-        return in_array($interface, class_implements($class));
-    }
+/**
+ * Check if class instance or string uses an specific interface.
+ *
+ * @param object|string $class
+ * @param string $interface
+ * @return bool
+ */
+function class_implement($class, string $interface)
+{
+    return in_array($interface, class_implements($class));
 }
 
-if (! function_exists('class_use')) {
-    /**
-     * Check if class instance or string uses an specific trait.
-     *
-     * @param object|string $class
-     * @param string $trait
-     * @param bool $recursive
-     * @return bool
-     */
-    function class_use($class, string $trait, bool $recursive = false)
-    {
-        return in_array($trait, $recursive ? class_uses_recursive($class) : class_uses($class));
-    }
+/**
+ * Check if class instance or string uses an specific trait.
+ *
+ * @param object|string $class
+ * @param string $trait
+ * @param bool $recursive
+ * @return bool
+ */
+function class_use($class, string $trait, bool $recursive = false)
+{
+    return in_array($trait, $recursive ? class_uses_recursive($class) : class_uses($class));
 }
--- src/Strings/functions.php
+++ src/Strings/functions.php
@@ -2,22 +2,20 @@
 
 namespace OpenSoutheners\LaravelHelpers\Strings;
 
-if (! function_exists('is_json')) {
-    /**
-     * Check if string is a valid JSON structure.
-     *
-     * @param string $string
-     * @return bool
-     */
-    function is_json(string $string)
-    {
-        if (
-            null === \json_decode($string, false, 512, JSON_UNESCAPED_UNICODE)
-            && JSON_ERROR_NONE !== \json_last_error()
-        ) {
-            return false;
-        }
-
-        return true;
+/**
+ * Check if string is a valid JSON structure.
+ *
+ * @param string $string
+ * @return bool
+ */
+function is_json(string $string)
+{
+    if (
+        null === \json_decode($string, false, 512, JSON_UNESCAPED_UNICODE)
+        && JSON_ERROR_NONE !== \json_last_error()
+    ) {
+        return false;
     }
+
+    return true;
 }
